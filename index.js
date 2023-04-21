2f98d952ab163945df9725e91261e6ab9128c441
Palakgupta2002 <pg948303@gmail.com>
2023-04-22 00:56:30 +0530

    Final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const boolean=true;
 var typewords=document.getElementById("mywords");
 var sentencebtn=document.getElementById("btn");
 var resultbtn=document.getElementById("Resultbutton");
+var msg=document.getElementById("msg");
 sentencebtn.addEventListener("click",()=>{
     if(boolean==true){
     getRandomSentence().then((sentense)=>{
@@ -11,31 +12,63 @@ sentencebtn.addEventListener("click",()=>{
         starttime=date.getTime();
         sentencebtn.innerText="done";
         document.getElementById("result").innerText="";
+       
     }).catch((error)=>{
         document.getElementById("msg").innerText="Error occur to fetching";
     })
-
 }
-boolean=false;
+
 
 })
-resultbtn.addEventListener("click",()=>{
-    if(boolean==false){
-    getRandomresult().then(()=>{
-        document.getElementById("msg").innerHTML=" "; 
-    }).catch((error)=>{
-        document.getElementById("result").innerText="Error occur to fetching result";
-    })
-}
-getRandomSentence();
+function Wordcounter(str) {
+    if (str === undefined || str === null) {
+        console.log("Invalid input: string is undefined or null");
+        return 0;
+    }
 
-})
+    let response = str.split(" ").length;
+    console.log(response);
+    return response;
+}
+
+resultbtn.addEventListener("click", () => {
+    document.getElementById("msg").innerHTML = "";
+    sentencebtn.innerText = "Start";
+    let date = new Date();
+    endtime = date.getTime();
+    let totaltime = ((endtime - starttime) / 1000);
+    console.log(totaltime, "hello");
+    let totalstr = typewords.value;
+    let wordcount = Wordcounter(totalstr);
+    let speed=Math.round((wordcount/totaltime)*60);
+    let finalmsg="your total speed at"+speed+"in a "+"at per minute";
+    finalmsg+=comparewords(msg.innerText,totalstr);
+    msg.innerText=finalmsg;
+    msg.style.color="black";
+    console.log(finalmsg);
+});
+function comparewords(str1, str2) {
+    if (str2 === undefined || str2 === null) {
+        console.log("Invalid input: string is undefined or null");
+        return 0;
+    }
+
+    let words1 = str1.split(" ");
+    let words2 = str2.split(" ");
+    let cnt = 0;
+    words1.forEach(function(index, item) {
+        if (item == words2[index]) {
+            cnt++;
+        }
+    });
+    let errorwords = words1.length - cnt;
+    return "  " + cnt +"   " +"  correct out of " +"  "+ words1.length +"  " + "   words and total number of errors are  " +" "+ errorwords + ".";
+}
 
 
 async function getRandomSentence() {
   const response = await fetch('https://api.quotable.io/random');
   const data = await response.json();
-  console.log(data);
   return data.content;
   
 }
